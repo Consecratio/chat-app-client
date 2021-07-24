cc4c5f6b7f07a37b59dddc71d74f93a30006683e
Consecratio <jcasique@gmail.com>
2021-07-24 12:09:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client'
 
 function App() {
+  const socket = io(process.env.REACT_APP_SERVER_URL)
+
+  socket.on('connection', () => {
+    console.log('Im connected with the backend')
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
