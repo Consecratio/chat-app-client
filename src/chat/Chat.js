ac2c5e249a1a181c6206a0e925b3e21af9f5ba78
Consecratio <jcasique@gmail.com>
2021-07-24 20:38:18 -0700

    receiving channels from server

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ChannelList from './ChannelList'
 import MessagesPanel from './MessagesPanel'
+import axios from 'axios'
 import './chat.css'
 
 const Chat = () => {
@@ -10,6 +11,16 @@ const Chat = () => {
         name: 'first',
         participants: 10
     }])
+
+    useEffect (() => {
+        const loadChannels = async () => {
+            let res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/getChannels`)
+
+            setChannels(res.data.channels)
+        }
+
+        loadChannels()
+    }, [])
 
     return (
         <div className="chat-app">
